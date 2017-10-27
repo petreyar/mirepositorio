5c50ead9ca4709da21b840841fd5dc2d4bb6098b
astudillo04 <marcelo.astudillo04@outlook.com>
2017-10-27 19:38:44 -0300

    first commit

---
+++ src/main/java/com/basic/example/helloworld.java
@@ -0,0 +1,13 @@
+package com.basic.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class helloworld 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
